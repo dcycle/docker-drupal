2aa02f36aeaf178f8868cc9831edf8ad95dc5017
alberto56 <albert@mediatribe.net>
2017-09-29 16:59:10 -0400

    Script should not know about git

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -16,8 +16,6 @@ else
   source "$CREDENTIALS";
 fi
 
-git checkout 8
-git pull origin 8
 DATE=`date '+%Y-%m-%d-%H-%M-%S-%Z'`
 docker build -t dcycle/drupal:8 .
 docker build -t dcycle/drupal:8.$DATE .
