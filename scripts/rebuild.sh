16d1fec5c6be5f42ce4145a9e5310f5b057108f4
alberto56 <albert@mediatribe.net>
2017-09-29 16:42:53 -0400

    Added script

---
+++ scripts/rebuild.sh
@@ -0,0 +1,25 @@
+# Rebuild script
+# This is meant to be run on a regular basis, daily or twice daily,
+# to rebuild the image with the latest security features.
+
+set -e
+
+CREDENTIALS="$HOME/.dcycle-docker-credentials.sh"
+
+if [ ! -f "$CREDENTIALS" ]; then
+  echo "Please create $CREDENTIALS and add to it:"
+  echo "DOCKERHUBUSER=xxx"
+  echo "DOCKERHUBPASS=xxx"
+  exit;
+else
+  source "$CREDENTIALS";
+fi
+
+git checkout 8
+git pull origin 8
+DATE=`date '+%Y-%m-%d-%H-%M-%S-%Z'`
+docker build -t dcycle/drupal:8 .
+docker build -t dcycle/drupal:8.$DATE .
+docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
+docker push dcycle/drupal:8
+docker push dcycle/drupal:8."$DATE"
