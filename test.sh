8be2429bce82bc281fc09c0d99b4b9ecc1627a8b
alberto56 <albert@mediatribe.net>
2019-12-18 17:22:39 -0500

    Please squash this commit in the pull request

--- test.sh
+++ test.sh
@@ -6,10 +6,15 @@ set -e
 docker pull drupal:8
 docker pull drupal:7
 
+echo "Testing Dockerfile-7"
 docker build -f="Dockerfile-7" -t test-drupal-7 .
+echo "Testing Dockerfile-8"
 docker build -f="Dockerfile-8" -t test-drupal-8 .
+echo "Testing Dockerfile-8drush9"
 docker build -f="Dockerfile-8drush9" -t test-drupal-8drush9 .
+echo "Testing Dockerfile-8drush"
 docker build -f="Dockerfile-8drush" -t test-drupal-8drush .
+echo "Testing Dockerfile-9"
 docker build -f="Dockerfile-9" -t test-drupal-9 .
 
 docker run --rm test-drupal-7 /bin/bash -c 'ls -lah | grep .htaccess'
