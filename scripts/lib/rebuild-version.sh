7eb60d55bdb2cb9fe56ac7595d08cd53c908e9f9
Albert Albala <albert@mediatribe.net>
2019-12-18 17:32:07 -0500

    drupal.org/node/3101956 Update composer file as per drupal/recommended-project, and provide feedback during rebuilding for better debugging (#8)

--- scripts/lib/rebuild-version.sh
+++ scripts/lib/rebuild-version.sh
@@ -1,6 +1,11 @@
 VERSION="$1"
 BASEIMAGEVERSION="$2"
 
+echo "*************"
+echo "** REBUILDING VERSION $1"
+echo "** USING BASE IMAGE $2"
+echo "*************"
+
 # Start by getting the latest version of the official drupal image
 docker pull drupal:"$BASEIMAGEVERSION"
 # Rebuild the entire thing
