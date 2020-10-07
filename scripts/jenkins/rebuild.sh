b6baa9097ff68e77e69ab6dfb53eb8b7bb665824
alberto56 <albert@mediatribe.net>
2020-10-07 17:19:52 -0400

    Please squash this commit in the pull request

--- scripts/jenkins/rebuild.sh
+++ scripts/jenkins/rebuild.sh
@@ -5,6 +5,7 @@ source ~/.docker-host-ssh-credentials
 # Create a droplet
 PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
   "./digitalocean/scripts/new-droplet.sh docker-drupal")
+# https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
 IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
 echo "Created Droplet at $IP"
 sleep 90
