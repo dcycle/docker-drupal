620c39ed6e57af87a23c06468506e0badae77219
Albert Albala <albert@mediatribe.net>
2021-11-16 22:17:27 -0500

    Add M1 chip support

--- scripts/lib/rebuild-version.sh
+++ scripts/lib/rebuild-version.sh
@@ -8,11 +8,7 @@ echo "*************"
 
 # Start by getting the latest version of the official drupal image
 docker pull drupal:"$BASEIMAGEVERSION"
+
 # Rebuild the entire thing
-docker build -f="Dockerfile-$VERSION" --no-cache -t dcycle/drupal:"$VERSION" .
-docker build -f="Dockerfile-$VERSION" -t dcycle/drupal:"$VERSION".$DATE .
-docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
-docker push dcycle/drupal:"$VERSION"
-docker push dcycle/drupal:"$VERSION"."$DATE"
-docker rmi dcycle/drupal:"$VERSION"
-docker rmi dcycle/drupal:"$VERSION"."$DATE"
+docker buildx build -t dcycle/drupal:"$VERSION" --platform linux/amd64,linux/arm64/v8 --push .
+docker buildx build -t dcycle/drupal:"$VERSION.$DATE" --platform linux/amd64,linux/arm64/v8 --push .
