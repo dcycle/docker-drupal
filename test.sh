55a1bec55abec2c7fafe384e9cc48fa90b700862
Albert Albala <albert@mediatribe.net>
2021-11-16 23:36:12 -0500

    Stop building Drupal 8 images (#18)

--- test.sh
+++ test.sh
@@ -4,25 +4,15 @@
 set -e
 
 docker pull drupal:9
-docker pull drupal:8
 docker pull drupal:7
 
 echo "Testing Dockerfile-7"
 docker build -f="Dockerfile-7" -t test-drupal-7 .
-echo "Testing Dockerfile-8"
-docker build -f="Dockerfile-8" -t test-drupal-8 .
-echo "Testing Dockerfile-8drush9"
-docker build -f="Dockerfile-8drush9" -t test-drupal-8drush9 .
-echo "Testing Dockerfile-8drush"
-docker build -f="Dockerfile-8drush" -t test-drupal-8drush .
 echo "Testing Dockerfile-9"
 docker build -f="Dockerfile-9" -t test-drupal-9 .
 echo "Testing Dockerfile-9php8"
 docker build -f="Dockerfile-9php8" -t test-drupal-9php8 .
 
 source ./scripts/lib/smoke-test.sh test-drupal-7
-source ./scripts/lib/smoke-test.sh test-drupal-8
-source ./scripts/lib/smoke-test.sh test-drupal-8drush9
-source ./scripts/lib/smoke-test.sh test-drupal-8drush
 source ./scripts/lib/smoke-test.sh test-drupal-9
 source ./scripts/lib/smoke-test.sh test-drupal-9php8
