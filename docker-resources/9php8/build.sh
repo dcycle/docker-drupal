2fdb7aa2d7f99ce3e2fcc6b1b66c8d641e5cd8c6
Albert Albala <albert@mediatribe.net>
2021-12-31 10:56:12 -0500

    Please squash this commit in the pull request

--- docker-resources/9php8/build.sh
+++ docker-resources/9php8/build.sh
@@ -5,7 +5,7 @@
 set -e
 
 mkdir -p /composer-file
-cp docker-resources/9php8/composer.json /composer-file/composer.json
+cp /docker-resources/9php8/composer.json /composer-file/composer.json
 # See "/var/www/html vs /opt/drupal" in ./README.md
 # This code exists in ./Dockerfile-8, ./Dockerfile-8drush, ./Dockerfile-9
 rm /var/www/html
