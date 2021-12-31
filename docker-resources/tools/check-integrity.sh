a86fc07403b79ac6bface2a01a5c446adf291a33
Albert Albala <albert@mediatribe.net>
2021-12-31 09:31:40 -0500

    Please squash this commit in the pull request

--- docker-resources/tools/check-integrity.sh
+++ docker-resources/tools/check-integrity.sh
@@ -3,7 +3,15 @@
 # Making sure that certain files exist and work as expected.
 #
 
+echo ">"
+echo "=>"
+echo "==>"
+echo "===>"
 echo "====> INTEGRITY CHECK $1"
 echo "$PATH"
 find / -name ldconfig
 echo "====> END INTEGRITY CHECK $1"
+echo "===>"
+echo "==>"
+echo "=>"
+echo ">"
