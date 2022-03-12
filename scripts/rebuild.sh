3fad434b55382b24434632737548ac8fa1eedbac
Albert Albala <albert@mediatribe.net>
2022-03-12 17:50:47 -0500

    Please squash this commit in the pull request

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -18,22 +18,20 @@ fi
 
 DATE=`date '+%Y-%m-%d-%H-%M-%S-%Z'`
 
-if [ ! -d prepare-docker-buildx ]; then
-  # See https://github.com/dcycle/prepare-docker-buildx, for M1 native images.
-  git clone https://github.com/dcycle/prepare-docker-buildx.git
-  cd prepare-docker-buildx
-  export DOCKER_CLI_EXPERIMENTAL=enabled
-  ./scripts/run.sh
-  cd ..
+# See https://github.com/dcycle/prepare-docker-buildx, for M1 native images.
+git clone https://github.com/dcycle/prepare-docker-buildx.git
+cd prepare-docker-buildx
+export DOCKER_CLI_EXPERIMENTAL=enabled
+./scripts/run.sh
+cd ..
 
-  docker buildx create --name mybuilder
-  docker buildx use mybuilder
-  docker buildx inspect --bootstrap
-  docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
-fi
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx inspect --bootstrap
+docker login -u"$DOCKERHUBUSER" -p"$DOCKERHUBPASS"
 
 # Rebuild the entire thing because there may be security updates
 # since the last build.
-source ./scripts/lib/rebuild-version.sh 7 7 linux/amd64
-source ./scripts/lib/rebuild-version.sh 9php8 9 linux/amd64
-source ./scripts/lib/rebuild-version.sh 9 8 linux/arm64/v8,linux/amd64
+source ./scripts/lib/rebuild-version.sh 7 7 linux/amd64,linux/arm64/v8
+source ./scripts/lib/rebuild-version.sh 9 8 linux/amd64,linux/arm64/v8
+source ./scripts/lib/rebuild-version.sh 9php8 9 linux/amd64,linux/arm64/v8
--- test.sh
+++ test.sh
@@ -8,11 +8,8 @@ docker pull drupal:7
 
 echo "Testing Dockerfile-7"
 docker build -f="Dockerfile-7" -t test-drupal-7 .
-echo "Testing Dockerfile-9"
-docker build -f="Dockerfile-9" -t test-drupal-9 .
 echo "Testing Dockerfile-9php8"
 docker build -f="Dockerfile-9php8" -t test-drupal-9php8 .
 
 source ./scripts/lib/smoke-test.sh test-drupal-7
-source ./scripts/lib/smoke-test.sh test-drupal-9
 source ./scripts/lib/smoke-test.sh test-drupal-9php8
