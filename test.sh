f8c96418f1a58cf44ee86cb33a4b7609a867e729
Albert Albala <albert@mediatribe.net>
2022-09-07 14:19:45 -0400

    Please squash this commit in the pull request

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@
 #
 set -e
 
+docker pull drupal:10
 docker pull drupal:9
 docker pull drupal:7
 
@@ -11,6 +12,10 @@ echo "Testing Dockerfile-7"
 docker build -f="Dockerfile-7" -t test-drupal-7 .
 echo "Testing Dockerfile-9php8"
 docker build -f="Dockerfile-9php8" -t test-drupal-9php8 .
+echo "Testing Dockerfile-10-fpm-alpine"
+docker build -f="Dockerfile-10-fpm-alpine" -t test-drupal-10-fpm-alpine .
+echo "Testing Dockerfile-10-fpm-alpine-dev"
+docker build -f="Dockerfile-10-fpm-alpine-dev" -t test-drupal-10-fpm-alpine-dev .
 
 ./scripts/lib/test.sh 7
 ./scripts/lib/test.sh 9php8
