cfa9d69b33dad02680c7707b0856f71ae7057fd0
Albert Albala <albert@mediatribe.net>
2022-12-27 12:03:19 -0500

    Please squash this commit in the pull request

--- docker-resources/9php8-fpm-alpine-dev/build.sh
+++ docker-resources/9php8-fpm-alpine-dev/build.sh
@@ -16,7 +16,10 @@ echo 'opcache.enable=0' >> /usr/local/etc/php/php.ini
 # https://stackoverflow.com/questions/46825502/how-do-i-install-xdebug-on-dockers-official-php-fpm-alpine-image
 apk add --no-cache --virtual .build-deps $PHPIZE_DEPS
 pecl install xdebug
-docker-php-ext-enable xdebug
+# We need to append -stable in order to prevent "error: rtnetlink.h is required"
+# See
+# https://github.com/mlocati/docker-php-extension-installer/issues/613#issuecomment-1191214473
+docker-php-ext-enable xdebug-stable
 apk del -f .build-deps
 
 echo "xdebug.start_with_request=trigger" >> /usr/local/etc/php/conf.d/xdebug.ini
