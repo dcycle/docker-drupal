e59eb669da1d440cf12a1c00f8de378f39d8f647
Albert Albala <albert@mediatribe.net>
2022-12-27 20:10:51 -0500

    prevent error "rtnetlink.h is required" (#37)

--- docker-resources/9php8-fpm-alpine-dev/build.sh
+++ docker-resources/9php8-fpm-alpine-dev/build.sh
@@ -14,10 +14,11 @@ echo 'opcache.enable=0' >> /usr/local/etc/php/php.ini
 # See https://matthewsetter.com/setup-step-debugging-php-xdebug3-docker/
 # https://github.com/docker-library/php/issues/412#issuecomment-297170197
 # https://stackoverflow.com/questions/46825502/how-do-i-install-xdebug-on-dockers-official-php-fpm-alpine-image
-apk add --no-cache --virtual .build-deps $PHPIZE_DEPS
+# linux-headers required to prevent "error: rtnetlink.h is required"
+apk add --no-cache --virtual .build-deps --update \
+  linux-headers autoconf $PHPIZE_DEPS
 pecl install xdebug
 docker-php-ext-enable xdebug
-apk del -f .build-deps
 
 echo "xdebug.start_with_request=trigger" >> /usr/local/etc/php/conf.d/xdebug.ini
 echo "xdebug.mode=profile" >> /usr/local/etc/php/conf.d/xdebug.ini
